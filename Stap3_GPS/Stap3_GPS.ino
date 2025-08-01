89bcb277ee98ef67dfffa7990591d3538b56e526
lathoub <4082369+lathoub@users.noreply.github.com>
2025-08-01 16:40:21 +0200

    add gps ino

--- Stap3_GPS/Stap3_GPS.ino
+++ Stap3_GPS/Stap3_GPS.ino
@@ -1,1 +1,27 @@
+#include <TinyGPSPlus.h>
+
+TinyGPSPlus gps;
+
+#define RXD1 20
+#define TXD1 21 // optional
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial){}
+  delay(1000);
+  Serial.println("Booting");
+
+  Serial1.begin(9600, SERIAL_8N1, RXD1, TXD1);
+}
+
+void loop() {
+  while (Serial1.available()) {
+    gps.encode(Serial1.read());
+  }
+
+  if (gps.location.isUpdated()) {
+    Serial.print("Lat: "); Serial.println(gps.location.lat(), 6);
+    Serial.print("Lng: "); Serial.println(gps.location.lng(), 6);
+  }
+}
 
